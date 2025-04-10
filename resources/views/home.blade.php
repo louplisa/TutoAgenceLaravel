741538f8400e1673468659ebd3b4ea489e7c66c8
aurelie <ferre.aurelie@wanadoo.fr>
2025-04-10 15:18:37 +0200

    Blade component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,20 @@
 @extends('base')
 
 @section('content')
+    <x-alert type="success" class="fw-bold">
+        Des informations
+    </x-alert>
     <div class="bg-light p-5 mb-5 text-center">
         <div class="container">
             <h1>Agence lorem ipsum</h1>
-            <p>Nullam nec turpis et arcu egestas commodo. Integer sit amet metus non tortor tincidunt interdum. Donec et metus mollis, ultricies est at, ultricies nulla. Morbi non libero magna. Praesent imperdiet magna ac ipsum cursus, ut fermentum turpis tincidunt.
-                Praesent placerat, magna in vehicula vestibulum, felis urna cursus lorem, sed vestibulum quam eros vel libero. Vivamus commodo, odio sed fringilla pretium, sem nulla feugiat odio, in cursus elit dolor et ex.
-                Mauris vitae quam in justo dictum sodales. In eget tortor a nunc vehicula tempor. Nam ac tincidunt ipsum, eget accumsan nisi. Praesent porta, magna vitae dapibus pharetra, erat eros efficitur nunc, in mattis lectus libero a velit. Nulla facilisi.
+            <p>Nullam nec turpis et arcu egestas commodo. Integer sit amet metus non tortor tincidunt interdum. Donec et
+                metus mollis, ultricies est at, ultricies nulla. Morbi non libero magna. Praesent imperdiet magna ac
+                ipsum cursus, ut fermentum turpis tincidunt.
+                Praesent placerat, magna in vehicula vestibulum, felis urna cursus lorem, sed vestibulum quam eros vel
+                libero. Vivamus commodo, odio sed fringilla pretium, sem nulla feugiat odio, in cursus elit dolor et ex.
+                Mauris vitae quam in justo dictum sodales. In eget tortor a nunc vehicula tempor. Nam ac tincidunt
+                ipsum, eget accumsan nisi. Praesent porta, magna vitae dapibus pharetra, erat eros efficitur nunc, in
+                mattis lectus libero a velit. Nulla facilisi.
             </p>
         </div>
     </div>
