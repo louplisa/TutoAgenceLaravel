b7f39d9dc727352c8caf8ae946bd4c77697a15d2
aurelie <ferre.aurelie@wanadoo.fr>
2025-04-11 14:53:37 +0200

    Internationalisation

--- resources/views/property/show.blade.php
+++ resources/views/property/show.blade.php
@@ -39,7 +39,7 @@
                 <hr>
 
                 <div class="mt-4">
-                    <h4>Intéressé par ce bien ?</h4>
+                    <h4>{{ __('Interested in the property :title ?', ['title' => $property->title]) }}</h4>
 
                     @include('shared.flash')
 
